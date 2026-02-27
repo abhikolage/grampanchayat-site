27ead1e3ed9f563d454c6c6382f1a77c8b214933
Abhijit Kolage <abhijit.kolage@nitorinfotech.com>
2026-02-27 21:30:45 +0530

    Updated routerlinks for school, notices and services pages

--- grampanchayat-site-avhane-bk/src/app/Pages/complaints/complaints.ts
+++ grampanchayat-site-avhane-bk/src/app/Pages/complaints/complaints.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-complaints',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './complaints.html',
   styleUrl: './complaints.css',
 })
