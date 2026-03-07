8d1e61d6f57c3c15f3037abdcd8cad64d525d72c
Abhijit Kolage <abhikolage1995@gmail.com>
2026-03-07 20:20:11 +0530

    Updated code to call API for contact and complaints

---
+++ grampanchayat-site-avhane-bk/src/app/Services/contact.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ContactModel } from '../Models/contact.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  private apiUrl = "https://grampanchayat-avhane-bk-api.onrender.com/api/contact";
+
+  constructor(private http: HttpClient) {}
+
+  sendContact(data: ContactModel) {
+    return this.http.post(this.apiUrl, data);
+  }
+}
