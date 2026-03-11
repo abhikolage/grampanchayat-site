c00a726fc2787d37d750cba726bf9fa5fd9a52c8
Abhijit Kolage <abhikolage1995@gmail.com>
2026-03-11 20:22:11 +0530

    Added new config file for all data and dynamically binding in html

--- grampanchayat-site-avhane-bk/src/app/Pages/about/about.ts
+++ grampanchayat-site-avhane-bk/src/app/Pages/about/about.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SiteConfigService } from '../../Services/site-config.service';
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
+  config: any;
+  constructor(private configService: SiteConfigService) { }
 
+  ngOnInit() {
+
+    this.config = this.configService.getConfig();
+
+  }
 }
--- grampanchayat-site-avhane-bk/src/app/Pages/contact/contact.ts
+++ grampanchayat-site-avhane-bk/src/app/Pages/contact/contact.ts
@@ -5,6 +5,8 @@ import { ContactService } from '../../Services/contact.service';
 import { ContactModel } from '../../Models/contact.model';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef } from '@angular/core';
+import { SiteConfigService } from '../../Services/site-config.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 
 @Component({
@@ -15,11 +17,15 @@ import { ChangeDetectorRef } from '@angular/core';
   styleUrl: './contact.css',
 })
 export class Contact {
-constructor(private contactService: ContactService, private cdr: ChangeDetectorRef) {}
 
- loading = false;
-success = false;
-errorMessage = '';
+  config: any;
+  safeMapUrl!: SafeResourceUrl;
+  constructor(private contactService: ContactService, private cdr: ChangeDetectorRef, private configService: SiteConfigService,
+    private sanitizer: DomSanitizer) { }
+
+  loading = false;
+  success = false;
+  errorMessage = '';
 
   contactForm: ContactModel = {
     name: '',
@@ -29,57 +35,67 @@ errorMessage = '';
     message: ''
   };
 
-submitContact() {
 
-  if (!this.contactForm.name || !this.contactForm.mobile || !this.contactForm.message) {
-    this.errorMessage = "कृपया सर्व आवश्यक माहिती भरा";
-    return;
+  ngOnInit() {
+
+    this.config = this.configService.getConfig();
+    this.safeMapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.config.contactPage.map.embedUrl
+    );
+
   }
 
-  this.loading = true;
-  this.success = false;
-  this.errorMessage = '';
+  submitContact() {
 
-  this.contactService.sendContact(this.contactForm).subscribe({
-
-    next: () => {
-
-      this.loading = false;
-      this.success = true;
-
-      this.contactForm = {
-        name: '',
-        mobile: '',
-        email: '',
-        subject: 'सामान्य चौकशी',
-        message: ''
-      };
-
-       this.cdr.detectChanges();
-
-      setTimeout(() => {
-        this.success = false;
-        this.cdr.detectChanges();
-      }, 5000);
-
-    },
-
-    error: () => {
-
-      this.loading = false;
-      this.errorMessage = "संदेश पाठवताना त्रुटी आली";
-
-       this.cdr.detectChanges();
-
-      setTimeout(() => {
-        this.errorMessage = '';
-        this.cdr.detectChanges();
-      }, 5000);
-
+    if (!this.contactForm.name || !this.contactForm.mobile || !this.contactForm.subject || !this.contactForm.message) {
+      this.errorMessage = this.config?.contactPage?.form?.formErrorMessage;
+      return;
     }
 
-  });
+    this.loading = true;
+    this.success = false;
+    this.errorMessage = '';
 
-}
+    this.contactService.sendContact(this.contactForm).subscribe({
+
+      next: () => {
+
+        this.loading = false;
+        this.success = true;
+
+        this.contactForm = {
+          name: '',
+          mobile: '',
+          email: '',
+          subject: 'सामान्य चौकशी',
+          message: ''
+        };
+
+        this.cdr.detectChanges();
+
+        setTimeout(() => {
+          this.success = false;
+          this.cdr.detectChanges();
+        }, 5000);
+
+      },
+
+      error: () => {
+
+        this.loading = false;
+        this.errorMessage = this.config?.contactPage?.form?.sendErrorMessage;
+
+        this.cdr.detectChanges();
+
+        setTimeout(() => {
+          this.errorMessage = '';
+          this.cdr.detectChanges();
+        }, 5000);
+
+      }
+
+    });
+
+  }
 
 }
