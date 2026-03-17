188e74f39396d60658636fe57065af9f880148a3
Abhijit Kolage <abhijit.kolage@nitorinfotech.com>
2026-03-17 17:00:14 +0530

    updated folder name in angular.json and package.json

--- grampanchayat-site-ui/src/app/app.spec.ts
+++ grampanchayat-site-ui/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, grampanchayat-site-avhane-bk');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, grampanchayat-site-ui');
   });
 });
